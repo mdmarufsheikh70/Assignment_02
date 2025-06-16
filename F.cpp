8cae5d26fadbce158c7452a20ef33fe98dee4521
Maruf <mdmarufsheikh707@gmail.com>
2025-06-16 12:16:41 +0600

    Add files via upload

---
+++ F.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main()
+{
+    optimize();
+    
+    int x, y;
+    cin >> x >> y;
+
+    if(x >= 60 && y <= 130)
+    {
+        cout << "YES" << endl;
+    } 
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
