cbf3028482a3b5118ad9b36acc76cc3deba149ae
Maruf <mdmarufsheikh707@gmail.com>
2025-06-19 20:37:02 +0600

    Add files via upload

---
+++ L.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main()
+{
+    optimize();
+    int n;
+    cin >> n;
+
+    if(n >= 8)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
